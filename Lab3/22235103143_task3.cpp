6c44b7bd608b671eda5657f6b6a404d3f679526a
developernahid <nahidhasan856059@gmail.com>
2024-01-19 18:29:52 +0600

    Lab-3

--- Lab3/22235103143_task3.cpp
+++ Lab3/22235103143_task3.cpp
@@ -1,21 +1,12 @@
-#include <iostream>
-#include <cstring>
-
+#include <bits/stdc++.h>
 using namespace std;
-
 bool isPangram(const string& str) {
-    // Array to keep track of the occurrence of each alphabet
     bool alphabets[26] = {false};
-
-    // Traverse the input string
     for (char ch : str) {
         if (isalpha(ch)) {
-            // Convert the character to lowercase and mark its occurrence
             alphabets[tolower(ch) - 'a'] = true;
         }
     }
-
-    // Check if all alphabets have occurred at least once
     for (bool isPresent : alphabets) {
         if (!isPresent) {
             return false;
@@ -27,16 +18,12 @@ bool isPangram(const string& str) {
 
 int main() {
     string input;
-
-    // Get input from the user
     cout << "Enter a string: ";
     getline(cin, input);
-
-    // Check if the input is a Pangram
     if (isPangram(input)) {
-        cout << "The given string is a Pangram." << endl;
+        cout << "YES" << endl;
     } else {
-        cout << "The given string is not a Pangram." << endl;
+        cout << "NO" << endl;
     }
 
     return 0;
